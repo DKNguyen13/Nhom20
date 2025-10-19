3e7f41446d177bd9ab0ac2462109e2e7e8e5084d
nguyen.dk <nd2004lk13@gmail.com>
2025-10-19 18:15:22 +0700

    fix: fix bug update price vip package

--- toeic-web/src/pages/Admin/VipManagement/VipManagement.tsx
+++ toeic-web/src/pages/Admin/VipManagement/VipManagement.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import { FaSave } from "react-icons/fa";
 import LeftSidebarAdmin from "../../../components/LeftSidebarAdmin";
 import api from "../../../config/axios";
+import { toast, ToastContainer } from "react-toastify";
 
 interface Package {
-  _id: string; // BE trả về sẽ có _id
+  _id: string;
   name: string;
   type: "basic" | "pro" | "premium";
   durationMonths: number;
@@ -54,15 +55,11 @@ const VipManagementPage: React.FC = () => {
 
   const handleSave = async (pkg: Package) => {
     try {
-      await api.put(`/vip/${pkg._id}`, {
-        originalPrice: pkg.originalPrice,
-        discountedPrice: pkg.discountedPrice,
-        description: pkg.description,
-      });
-      alert(`Lưu thành công gói ${pkg.name}`);
+      await api.put(`/vip/${pkg._id}`, { originalPrice: pkg.originalPrice, discountedPrice: pkg.discountedPrice, description: pkg.description });
+      toast.success(`Lưu thành công gói ${pkg.name}`);
     } catch (err: any) {
-      console.error("Lỗi cập nhật:", err);
-      alert("Cập nhật thất bại");
+      const message = err.response?.data?.message || "Lỗi khi cập nhật gói VIP";
+      toast.error(`${message}`);
     }
   };
 
@@ -75,13 +72,14 @@ const VipManagementPage: React.FC = () => {
       {/* Sidebar */}
       <LeftSidebarAdmin customHeight="h-auto w-64" />
 
+      <ToastContainer position="top-right" autoClose={1500} />
+
       {/* Main Content */}
       <div className="flex-1 p-8">
         <h1 className="text-3xl font-bold mb-6 text-gray-800">Quản lý gói VIP</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {packages.map((pkg, index) => (
-            <div
-              key={pkg._id}
+            <div key={pkg._id}
               className="bg-white p-5 rounded-lg shadow hover:shadow-lg transition"
             >
               <h3 className="text-xl font-semibold mb-2">{pkg.name}</h3>
@@ -91,11 +89,8 @@ const VipManagementPage: React.FC = () => {
                 <label className="block text-sm font-medium text-gray-700">
                   Mô tả:
                 </label>
-                <textarea
-                  value={pkg.description || ""}
-                  onChange={(e) =>
-                    handleFieldChange(index, "description", e.target.value)
-                  }
+                <textarea value={pkg.description || ""}
+                  onChange={(e) => handleFieldChange(index, "description", e.target.value)}
                   className="mt-1 block w-full border border-gray-300 rounded px-2 py-1"
                   rows={2}
                 />
@@ -105,8 +100,7 @@ const VipManagementPage: React.FC = () => {
                 <label className="block text-sm font-medium text-gray-700">
                   Giá gốc:
                 </label>
-                <input
-                  type="number"
+                <input type="number" max={999999999}  
                   value={pkg.originalPrice}
                   onChange={(e) =>
                     handleFieldChange(
@@ -116,6 +110,12 @@ const VipManagementPage: React.FC = () => {
                     )
                   }
                   className="mt-1 block w-full border border-gray-300 rounded px-2 py-1"
+                  onInput={(e) => {
+                    const target = e.target as HTMLInputElement;
+                    if (target.value.length > 9) {
+                      target.value = target.value.slice(0, 9);
+                    }
+                  }}
                 />
               </div>
 
@@ -123,8 +123,7 @@ const VipManagementPage: React.FC = () => {
                 <label className="block text-sm font-medium text-gray-700">
                   Giá giảm:
                 </label>
-                <input
-                  type="number"
+                <input max={999999999} type="number"
                   value={pkg.discountedPrice}
                   onChange={(e) =>
                     handleFieldChange(
@@ -134,11 +133,16 @@ const VipManagementPage: React.FC = () => {
                     )
                   }
                   className="mt-1 block w-full border border-gray-300 rounded px-2 py-1"
+                  onInput={(e) => {
+                    const target = e.target as HTMLInputElement;
+                    if (target.value.length > 9) {
+                      target.value = target.value.slice(0, 9);
+                    }
+                  }}
                 />
               </div>
 
-              <button
-                onClick={() => handleSave(pkg)}
+              <button onClick={() => handleSave(pkg)}
                 className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 flex items-center justify-center gap-2"
               >
                 <FaSave /> Lưu
