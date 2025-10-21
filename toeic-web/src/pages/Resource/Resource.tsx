1638921f9c601b375a0b2e55c38e0ec9216ccc1a
nguyen.dk <nd2004lk13@gmail.com>
2025-10-21 20:17:08 +0700

    refactor: improve flow lesson

--- toeic-web/src/pages/Resource/Resource.tsx
+++ toeic-web/src/pages/Resource/Resource.tsx
@@ -1,4 +1,4 @@
-import api from "../../config/axios";
+import api, { isLoggedIn } from "../../config/axios";
 import React, { useState, useEffect } from "react";
 import ResourceCard from "../../components/ResourceCard";
 import LoadingSkeleton from "../../components/common/LoadingSpinner/LoadingSkeleton";
@@ -23,7 +23,8 @@ const ResourcePage: React.FC = () => {
   useEffect(() => {
     const fetchResources = async () => {
       try {
-        const res = await api.get("/lessons");
+        const endpoint = isLoggedIn() ? "/lessons" : "/lessons/public";
+        const res = await api.get(endpoint);
         setResources(res.data.data);
       } catch (err) {
         console.error("Lỗi load resources:", err);
